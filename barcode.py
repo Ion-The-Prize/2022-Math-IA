b1312d17912e1574402bec1cade22288b3dfa4b1
Bert B-L <bert@bee-lindgren.com>
2022-03-06 22:17:43 -0500

    Adding x-axis ticks and labels
    Also adding get_x_range which externalizes x-pixel locations

--- barcode.py
+++ barcode.py
@@ -2,14 +2,16 @@ from graphics import *
 import random
 
 def get_rand_color():
-   r=random.randrange(256)
-   b=random.randrange(256)
-   g=random.randrange(256)
+   r=random.randrange(25,256)
+   b=random.randrange(25,256)
+   g=random.randrange(25,256)
    color=color_rgb(r,b,g)
    return color
 
 WHITE=color_rgb(255,255,255)
 GREY=color_rgb(100,100,100)
+YELLOW=color_rgb(255,255,0)
+RED=color_rgb(255,0,0)
 
 class Bar:
     """A bar on the barcode"""
@@ -40,6 +42,7 @@ class Bar:
         line.draw(self.barcode.win)
 
 
+
 class BarCode:
     colors = []
 
@@ -47,6 +50,9 @@ class BarCode:
     x_min = None  # type: float
     x_max = None  # type: float
     x_range = None  # type: float
+
+    x_tick_marks=[] #type: list[float]
+
 
     y_min = None  # type: float
     y_max = None  # type: float
@@ -62,7 +68,7 @@ class BarCode:
     bars = {}
 
 
-    def __init__(self, title, _min_x, _max_x, _width=800, _height=300, num_colors=0):
+    def __init__(self, title, _min_x, _max_x, _width=800, _height=300, num_colors=0, _num_ticks_x=10):
       """
       Initialize the barcode library
 
@@ -75,6 +81,22 @@ class BarCode:
       self.title = title
       self.x_min = _min_x
       self.x_max = _max_x
+      self.num_ticks_x = _num_ticks_x
+
+      x_range = self.x_max - self.x_min
+
+      x_tick_first=None
+      x_tick_inc=None
+      if ( x_range>10 ):
+          #integer mode
+          x_tick_first= round(self.x_min)+1
+          x_tick_inc = round(x_range/10)
+      else:
+          x_tick_first=self.x_min
+          x_tick_inc=x_range/10
+      for i in range(self.num_ticks_x):
+          self.x_tick_marks.append(x_tick_first + i*x_tick_inc)
+
       self.win_width = _width
       self.win_height = _height
       self.win = GraphWin(str(title), self.win_width, self.win_height)
@@ -94,8 +116,8 @@ class BarCode:
         :param color: Color to draw
         :return:
         """
-        assert(color_num != None or color != None)
-        assert(color_num == None or color_num<len(self.colors))
+        assert(color_num is not None or color is not None)
+        assert(color_num is None or color_num<len(self.colors))
         assert(self.x_min<=x<=self.x_max)
 
         if (color_num):
@@ -135,6 +157,16 @@ class BarCode:
             bar=self.bars.get(x_pix)
             if ( bar != None ):
                 bar.draw()
+
+        for tick_x in self.x_tick_marks:
+            x_pix = (tick_x - self.x_min) * self.pix_per_x
+            line = Line(Point(x_pix, 0), Point(x_pix, self.win_height))
+            line.setFill(YELLOW)
+            line.draw(self.win)
+            label = Text(Point(x_pix-5, self.win_height-20), "{:.1f}".format(tick_x))
+            label.setFill(RED);
+            label.draw(self.win)
+
         self.win.flush()
 
     def window_is_open(self):
@@ -152,3 +184,6 @@ class BarCode:
 
     def close_on_click(self):
         self.close_on_click = True
+        
+    def get_x_range(self):
+        return range(self.x_min, self.x_max, 1.0/self.pix_per_x)
