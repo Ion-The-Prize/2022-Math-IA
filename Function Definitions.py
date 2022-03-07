03e5c1582b4c8c0675b0ab7afa15e45860baa17f
GhastlyGast <e@flyingdragon.net>
2022-03-06 20:24:14 -0500

    Added get_roots_with_dividing in Polynomial class
    Broke off get_roots_with_dividing from get_roots function because it was so different. More work still needs to be done, however. Also, some formatting was fixed

--- Function Definitions.py
+++ Function Definitions.py
@@ -25,7 +25,7 @@ class CalculatedRoot:
         self.starting_guess = starting_guess
         self.root_was_found = root_was_found
 
-    def __str__(self):
+    def __repr__(self):
         if self.root_was_found:
             return "x={:.3f} | steps={:d} | start={:.3f}".format(self.x_value , self.steps_taken , self.starting_guess)
         else:
@@ -42,7 +42,6 @@ class Polynomial:
     poly_roots = None  # None is you don't know the roots
     poly_degree = None
 
-
     def __init__(self , poly_coefficients_list , poly_roots = None):
         """
 
@@ -55,10 +54,8 @@ class Polynomial:
         self.poly_roots = poly_roots
         self.poly_degree = len(poly_coefficients_list) - 1
 
-
     def __eq__(self, other):
         return self.poly_coefficients_list == other.poly_coefficients_list
-
 
     def poly_printer(self , desmos_format = False):
         """
@@ -101,12 +98,10 @@ class Polynomial:
                         # To paste into desmos, the single curly braces are changed into triple curly braces
         return result
 
-
     def get_degree(self):
         """Returns int degree of the polynomial"""
 
         return self.poly_degree
-
 
     def multiply(self , polynomial_second = None):
         """
@@ -142,12 +137,11 @@ class Polynomial:
             result_a = [0] * (polynomial_second.get_degree() + 1)  # read docstring
             for sec_poly_pos in range(len(polynomial_second.poly_coefficients_list)):
                 result_a[sec_poly_pos] = polynomial_second.poly_coefficients_list[sec_poly_pos] * self.poly_coefficients_list[first_poly_pos]
-            # result_a = product of the sec_poly_pos'th coefficient of polynomial_second and every digit of polynomial_first
+                # result_a = product of the sec_poly_pos'th coefficient of polynomial_second and every digit of polynomial_first
             for i in range(len(result_a)):
                 # add result_a to the currect part of running total
                 result_coefficients_list[first_poly_pos + i] += result_a[i]
         return Polynomial(result_coefficients_list , result_roots)
-
 
     def add(self , *other_polynomials):
         """Adds polynomials, returns Polynomial sum"""
@@ -161,7 +155,6 @@ class Polynomial:
                 b = polynomial.poly_coefficients_list[poly_pos] if len(polynomial.poly_coefficients_list) > poly_pos else 0
                 result_coefficients_list[poly_pos] += b
         return Polynomial(result_coefficients_list)
-
 
     def subtract(self , subtracting_polynomial = None):
         """Subtracts subtracting_polynomial from self"""
@@ -177,7 +170,6 @@ class Polynomial:
         while len(result_coefficients_list) > 1 and result_coefficients_list[-1] == 0:
             del result_coefficients_list[-1]
         return Polynomial(result_coefficients_list)
-
 
     def divide(self , divisor = None):
         """
@@ -205,7 +197,6 @@ class Polynomial:
             remainder_polynomial = remainder_polynomial.subtract(poly_to_subtract)
         return result , remainder_polynomial
 
-
     def evaluate(self , x ):
         """
         Given an x value, computes y value of self
@@ -221,7 +212,6 @@ class Polynomial:
             result += self.poly_coefficients_list[i] * (x ** i)
         return result
 
-
     def poly_primer(self):
         """
         Differentiates a polynomial
@@ -235,7 +225,6 @@ class Polynomial:
             result_coefficients_list.append(self.poly_coefficients_list[i] * i)
         return Polynomial(result_coefficients_list)
 
-
     def get_linear_root(self):
         """Returns x-intercept of a LINEAR EQUATION"""
 
@@ -244,7 +233,6 @@ class Polynomial:
 
         root = (-1.0 * self.poly_coefficients_list[0]) / self.poly_coefficients_list[1]
         return root
-
 
     def get_tangent_line(self , x):
         """
@@ -261,7 +249,6 @@ class Polynomial:
         x_intercept = tangent_line.get_linear_root()
 
         return Polynomial([y_intercept , slope] , [x_intercept])
-
 
     def get_relative_extrema(self):
         """
@@ -289,7 +276,6 @@ class Polynomial:
                 raise NotPoly
         return rel_maxima , rel_minima
 
-
     def get_newton_root_from_point(self , starting_x , max_steps = 10 , epsilon = 1e-8):
         """
 
@@ -316,14 +302,13 @@ class Polynomial:
         else:
             return CalculatedRoot(current_guess , current_value , max_steps , starting_x , root_was_found = False)
 
-
-    def get_roots(self , max_steps = 10 , epsilon = 1e-8 , starting_guess_count = None ,
+    def get_roots(self , max_steps = 20 , epsilon = 1e-8 , starting_guess_count = None ,
                   random_starting_guesses = True , guess_range_min = -BUILD_BINOMIAL_RANGE - 1,
                   guess_range_max = BUILD_BINOMIAL_RANGE + 1, factor_roots = False , sort_roots = False):
         """
         Uses Newton's method for finding roots of higher order polynomials.
 
-        :param max_steps: the number of tangent lines used (default 10)
+        :param max_steps: the number of tangent lines used (default 20)
         :type max_steps: int
         :param starting_guess_count: the number of starting starting_guesses (default is degree of inputted polynomial)
         :type starting_guess_count: int
@@ -335,11 +320,12 @@ class Polynomial:
         :type guess_range_min: float
         :param guess_range_max: interval of guessing ("root range") maximum
         :type guess_range_max: float
-        :param factor_roots: whether found roots will be factored out, resulting in a list of the actual roots (will only factor out roots with y values below epsilon)
+        :param factor_roots: INCOMPATIBLE WITH EVENLY SPACED STARTING GUESSES... whether found roots will be factored out,
+            results in a list of the actual roots (will only factor out roots with y values below epsilon and will try again until all roots are found)
         :type factor_roots: bool
         :param sort_roots: whether the final roots string will be sorted from most negative to most positive (and repeats removed) (default False)
         :type sort_roots: bool
-        :return: tuple[list(CalculatedRoot (successful roots)) , list(CalculatedRoot (failed roots))]
+        :return: tuple[list[CalculatedRoot (successful roots)] , list[CalculatedRoot (failed roots)]]
         """
 
         if starting_guess_count is None:
@@ -365,6 +351,57 @@ class Polynomial:
             else:
                 failed_roots += [newton_result]
         return poly_roots , failed_roots
+
+    def get_roots_with_dividing(self , max_steps_per_root = 20 , max_steps_before_quitting = None, epsilon = 1e-10 , guess_range_min = -BUILD_BINOMIAL_RANGE - 1,
+                  guess_range_max = BUILD_BINOMIAL_RANGE + 1, sort_roots = False):
+        """
+        Uses Newton's method for finding roots of higher order polynomials. After finding a root, it is factored out
+            of the polynomial, resulting in all the actual roots with no extra repeats
+            (will only factor out roots with y values below epsilon)
+
+        :param max_steps_per_root: the number of tangent lines used (default 20)
+        :type max_steps_per_root: int
+        :param max_steps_before_quitting: the number of times it should loop without finding a root with y below epsilon
+            before giving up, defaults to degree^2. If 0, will keep trying until a root below epsilon is found
+        :type max_steps_before_quitting: int
+        :param epsilon: really small value, only roots with y values below epsilon will be counted as actual roots
+        :type epsilon: float
+        :param guess_range_min: interval of guessing ("root range") minimum
+        :type guess_range_min: float
+        :param guess_range_max: interval of guessing ("root range") maximum
+        :type guess_range_max: float
+        :param sort_roots: whether the final roots string will be sorted from most negative to most positive (and repeats removed) (default False)
+        :type sort_roots: bool
+        :return: triple[list[CalculatedRoot (successful roots)] , list[CalculatedRoot (failed roots)] , list[Polynomial (remainders)]]
+        """
+
+        if max_steps_before_quitting is None:
+            max_steps_before_quitting = self.poly_degree ** 2
+
+        poly_roots = []
+        failed_roots = []
+        remainders = []
+        factored_poly = self
+        loops = 0
+        while factored_poly.poly_degree > 0:
+            guess = guess_range_min + (guess_range_max - guess_range_min) * random.random()
+            newton_result = factored_poly.get_newton_root_from_point(guess , max_steps_per_root , epsilon)
+            if newton_result.root_was_found:
+                factor = make_polynomial_from_coefficients(-1.0 * newton_result.x_value , 1.0)
+                factored_poly , factor_remainder = factored_poly.divide(factor)  # remainder's only returned; never used
+                print("Factor         " , factor.poly_coefficients_list)
+                print("Remaining Poly:" , factored_poly.poly_coefficients_list)
+                print("Remainder:     " , factor_remainder.poly_coefficients_list)
+                poly_roots += [newton_result]
+                remainders.append(factor_remainder)
+                loops = 0
+            else:
+                failed_roots += [newton_result]
+                loops += 1
+            if loops >= max_steps_before_quitting != 0:
+                print("Could not find factorable root after trying ", loops, " times.")
+                break
+        return poly_roots , failed_roots ,remainders
 
 
     def poly_power(self , power , pascal = 0):
@@ -432,6 +469,9 @@ def randomly_build_a_binomial(rand_lower_bound = -BUILD_BINOMIAL_RANGE , rand_up
 
 def make_polynomial_from_coefficients(*coefficients):
     """
+    Makes a Polynomial from args coefficients
+
+    :param coefficients: coefficients for Polynomial; given in ascending order of x powers
     :type coefficients: float
     """
 
@@ -540,6 +580,7 @@ print("prime value @ x = 1: ", testing_polynomial.poly_primer().evaluate(1))
 print("tangent line @x = 1: ", testing_polynomial.get_tangent_line(1).poly_coefficients_list)
 print("x-intercept of line: ", testing_polynomial.get_tangent_line(1).poly_roots)
 
+
 def tangent_test(x , polynomial , answer):
     """
     Test of get_tangent_line
@@ -625,15 +666,15 @@ for i in range(10):
     print()
 print("=====================================================================================================")
 
-new_poly = poly_maker(9)
+new_poly = poly_maker(7)
 print(new_poly.poly_printer(desmos_format = True))
 print(new_poly.poly_printer())
 print()
-calc_roots , fail_roots = new_poly.get_roots(max_steps = 4096 , epsilon = 1e-6)
 print("Poly Coeff: ", new_poly.poly_coefficients_list)
 print("Real Roots: ", new_poly.poly_roots)
+calc_roots , fail_roots , remainders = new_poly.get_roots_with_dividing(max_steps_per_root = 4096 , max_steps_before_quitting = 1 , epsilon = 1e-9)
 print("Calc Roots: ", [root.x_value for root in calc_roots])
-print("Roots Step: ", [root.steps_taken for root in calc_roots])
+print("Root Steps: ", [root.steps_taken for root in calc_roots])
 print("Fail Roots: ", ', '.join(str(r) for r in fail_roots))
 
 """
@@ -646,36 +687,41 @@ print("Zeros at x = ", question_poly[1])
 # print(poly_printer(poly_power(10, pascal = 0)))
 
 
-def MakePolyAndBarcodeIt(poly_degree, minimum, maximum, window_width , epsilon = 1e-8):
+def BarcodePoly(polynomial , minimum , maximum , window_width , epsilon = 1e-8):
     """
 
-    :param poly_degree:
+    :param polynomial:
+    :type polynomial: Polynomial
     :param minimum:
+    :type minimum: float
     :param maximum:
+    :type maximum: float
     :param window_width:
+    :param epsilon:
+    :type epsilon: float
     :return:
     """
 
-    assert(maximum > minimum)
+    assert (maximum > minimum)
 
-    poly = poly_maker(poly_degree)
     increment = (maximum - minimum) / window_width
 
-    barcode.init(minimum , maximum , window_width , 200 , poly_degree+1)
+    barcode.init(minimum , maximum , window_width , 200 , polynomial.poly_degree + 1)
 
     for i in range(window_width):
         x = minimum + (i * increment)
-        root = poly.get_newton_root_from_point(starting_x = x , max_steps = 4096, epsilon = epsilon)
+        root = polynomial.get_newton_root_from_point(starting_x = x , max_steps = 4096 , epsilon = epsilon)
         if root:
-            for r in range(len(poly.poly_roots)):
-                if abs(root.x_value - poly.poly_roots[r]) < epsilon:
+            for r in range(len(polynomial.poly_roots)):
+                if abs(root.x_value - polynomial.poly_roots[r]) < epsilon:
                     barcode.add_bar(x , color_num = r , y = 10 * root.steps_taken)
                     break
-    for r in range(len(poly.poly_roots)):
-        barcode.add_bar(x = poly.poly_roots[r], color_num = poly_degree, y=10)
+    for r in range(len(polynomial.poly_roots)):
+        barcode.add_bar(x = polynomial.poly_roots[r] , color_num = polynomial.poly_degree , y = 10)
+
 
 input("Press Enter to continue...")
-MakePolyAndBarcodeIt(5 , -15 , 15 , 1100)
+BarcodePoly(new_poly , -15 , 15 , 1100)
 while True:
     barcode.draw()
     input("Press Enter to continue...")
